5c5e2f184aac0ee876b83372d7fca85195712613
quanta-98 <70939194+quanta-98@users.noreply.github.com>
2020-11-17 17:23:47 +0700

    quan-cm-17/11/2020

---
+++ hoplong_test/Test_api/Test_api/Content/js/AboutJs/about.js
@@ -0,0 +1,22 @@
+﻿app.controller('AboutCtrl', function ($scope, $http) {
+    // load danh sách nhân viên, lọc theo tên phòng ban
+    $scope.LoadListEmployee = function (maphong) {
+        var data = {
+            isadmin: false,
+            maphongban: maphong,
+            macongty: 'HOPLONG'
+        }
+        $http.post('http://sales.hoplong.com/api/Api_NhanVien/NhanVienPhongBan',data).then(function (response) {
+            $scope.listEmployee = response.data;
+        })
+    }
+    $scope.LoadListEmployee();
+
+    // xem chi tiết nhân viên
+    $scope.ViewDetailEmployee = function (username) {
+        $http.get('http://sales.hoplong.com/api/Api_NhanVien/GetChiTietNhanVien/' + username).then(function (response) {
+            $scope.viewDetailEmployee = response.data;
+            console.log($scope.viewDetailEmployee);
+        })
+    }
+})
---
+++ hoplong_test/Test_api/Test_api/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_api.Startup))]
+namespace Test_api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
