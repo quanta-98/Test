309880620702f03fd021ec248b406ff70f28d6be
quanta-98 <70939194+quanta-98@users.noreply.github.com>
2020-11-18 17:16:12 +0700

    quan-cm-18/11/2020

---
+++ hoplong_test/Test_api/Test_api/Content/js/Class/class.js
@@ -0,0 +1,26 @@
+﻿app.controller('ClassCtrl', function ($scope, $http) {
+    //Load Full Data
+    $scope.LoadAllData = function () {
+        $http.get('http://10.10.15.162:3001/people').then(function (response) {
+            $scope.listData = response.data;
+        })
+    }
+    $scope.LoadAllData();
+
+    // Thêm data
+    $scope.AddPeople = function (Firstname, Lastname) {
+        var data = {
+            firstname: Firstname,
+            lastname: Lastname
+        }
+        $http.post('http://10.10.15.162:3001/person', data).then(function (response) {
+            SuccessSystem('thành công');
+            $scope.LoadAllData();
+        })
+    }
+
+    //Xem chi tiết học sinh theo Id
+    $scope.ViewDetail = function () {
+        $http.get('http://10.10.15.162:3001/person/')
+    }
+})
---
+++ hoplong_test/Test_api/Test_api/Content/js/HighCharts/highchartsCtrl.js
@@ -0,0 +1,67 @@
+﻿app.controller("HighChartsCtrl", function ($scope, $http) {
+    //var today = new Date();
+    //var dd = today.getDate();
+    //var mm = today.getMonth() + 1;
+
+    //var yyyy = today.getFullYear();
+    //if (dd < 10) {
+    //    dd = '0' + dd;
+    //}
+    //if (mm < 10) {
+    //    mm = '0' + mm;
+    //}
+    //var today = dd + '/' + mm + '/' + yyyy;
+    //$scope.ngay = today;
+
+    $scope.loadChart = function () {
+        var data = {
+            //date: $scope.ngay,
+            date:'16/11/2020',
+            mcid: 1
+        }
+        $http.post(chartUrl + '/api/Api_Machine/QuantityChart', data).then(function (response) {
+            $scope.listChart = response.data;
+            Highcharts.chart('container', {
+                chart: {
+
+                    type: 'column',
+                },
+                title: {
+                    text: 'Solar Employment Growth by Sector, 2010-2016'
+                },
+
+                subtitle: {
+                    text: 'Bieu DO TEST'
+                },
+
+                yAxis: {
+                    title: {
+                        text: 'Number of Employees'
+                    }
+                },
+
+                xAxis: {
+                    categories: ["06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "00", "01", "02", "03", "04", "05", ],
+
+                },
+
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle'
+                },
+
+
+
+                series: [{
+                    name: $scope.listChart[0].name,
+                    data: $scope.listChart[0].data,
+                }],
+
+
+            });
+        });
+
+    }
+    $scope.loadChart(); //goi ra luon
+})
